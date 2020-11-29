94fa69c6fe27c9aa461a6753dda0c370cf7f9efe
Daniel McCoshen <upstagecoronet@gmail.com>
2020-11-28 18:19:05 -0600

    change path

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class config:
-    # RECEPIE_PATH = "/var/www/html/recepie_book"
-    RECEPIE_PATH = "/home/daniel/Documents/Coding/recepies"
+    RECEPIE_PATH = "/storeage/recepie_book"
+    # RECEPIE_PATH = "/home/daniel/Documents/Coding/recepies"
     CATEGORIES = ["Breakfast", "Main_Course", "Soup", "Dessert"]
